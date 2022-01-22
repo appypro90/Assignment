515acf9cb469affd339d2d1c4ed3572ecca07447
Arpita Sen <Arpita Sen@LAPTOP-LIGVQST5>
2022-01-22 11:42:06 +1100

    Initial commit.

---
+++ Backend/Bl/SlotService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dal;
+using Domain.ConfigModels;
+using Domain.Models;
+using Domain.ViewModels;
+using Microsoft.Extensions.Options;
+
+namespace Bl
+{
+    public class SlotService : ISlotService
+    {
+        private readonly IJsonDal _jsonDal;
+        private readonly SlotConfig _slotConfig;
+        private readonly JsonDataSource _jsonDataSource;
+
+        public SlotService(IOptions<SlotConfig> slotConfig, IOptions<JsonDataSource> jsonDataSource, IJsonDal jsonDal)
+        {
+            _jsonDal = jsonDal;
+            _slotConfig = slotConfig.Value;
+            _jsonDataSource = jsonDataSource.Value;
+        }
+
+        public async Task<List<TimeSlotModel>> GetSlots()
+        {
+            var slots = GenerateSlots();
+            var bookedSlots = await _jsonDal.LoadJson<List<BookedSlot>>(_jsonDataSource.BookedSlots);
+            slots.ForEach(slot =>
+                    slot.IsAvailable = !bookedSlots.Exists(b => 
+                        b.StartTimeHour == slot.StartTime.Hour && b.StartTimeMinute == slot.StartTime.Minute)
+            );
+            return slots;
+        }
+
+        private List<TimeSlotModel> GenerateSlots()
+        {
+            var slots = new List<TimeSlotModel>();
+            var startTime = DateTime.Today.Add(new TimeSpan(_slotConfig.StartSlotHour, _slotConfig.StartSlotMinute, 00));
+            var endTime = DateTime.Today.Add(new TimeSpan(_slotConfig.EndSlotHour, _slotConfig.EndSlotMinute, 00));
+
+            while (startTime <= endTime.Add(new TimeSpan(0, -_slotConfig.SlotDurationInMinutes, 00)))
+            {
+                slots.Add(new TimeSlotModel
+                {
+                    IsAvailable = true,
+                    StartTime = startTime
+                });
+                startTime = startTime.Add(new TimeSpan(0, _slotConfig.SlotDurationInMinutes, 00));
+            }
+
+            return slots;
+        }
+    }
+}
